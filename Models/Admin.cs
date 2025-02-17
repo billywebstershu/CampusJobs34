f9be5903ebaa5078f6db544530aa432087041658
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 17:05:57 +0000

    Delete Models/Admin.cs

--- Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-namespace CampusJobsProject___Group_34.Models
-{
-    public class Admin
-    {
-        public int Admin_ID { get; set; }
-        public int User_ID { get; set; }
-        public User User { get; set; }
-    }
-}
