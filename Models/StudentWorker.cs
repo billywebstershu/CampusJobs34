c375b9dc7360e6ae5bfa784f10207f8a29f5fc0f
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 17:07:57 +0000

    Delete Models/StudentWorker.cs

--- Models/StudentWorker.cs
+++
@@ -1,9 +0,0 @@
-namespace CampusJobsProject___Group_34.Models
-{
-    public class StudentWorker
-    {
-        public int VisaStatus_ID { get; set; }
-        public int Recruitment_ID { get; set; }
-        public Recruiter Recruiter { get; set; }
-    }
-}
