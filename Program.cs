c3f91f43e8a0daa3485bf0750be945e5a072a199
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 17:37:05 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+// Register the MySQL connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddTransient<IDbConnection>(_ => new MySqlConnection(connectionString));
 
 var app = builder.Build();
 
