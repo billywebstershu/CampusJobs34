3148789dd2c9efb31794a407e52a1a7d526f2db2
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 17:06:17 +0000

    Delete Models/Employee.cs

--- Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace CampusJobsProject___Group_34.Models
-{
-    public class Employee
-    {
-        public int Student_ID { get; set; }
-        public int Recruitment_ID { get; set; }
-        public Recruiter Recruiter { get; set; }
-    }
-}
