b1ae5907e5dc402f17c3d6cee1ce8c27adb00e06
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 16:40:58 +0000

    Create Recruiter.cs

---
+++ Models/Recruiter.cs
@@ -0,0 +1,9 @@
+namespace CampusJobsProject___Group_34.Models
+{
+    public class Recruiter
+    {
+        public int Recruitment_ID { get; set; }
+        public int User_ID { get; set; }
+        public User User { get; set; }
+    }
+}
