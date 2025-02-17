44285fd3355efb7f367528f9317599a2663fb037
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 17:38:08 +0000

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,40 @@
-using System.Diagnostics;
-using CampusJobsProject___Group_34.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace CampusJobsProject___Group_34.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IDbConnection _dbConnection;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IDbConnection dbConnection)
         {
-            _logger = logger;
+            _dbConnection = dbConnection;
         }
 
         public IActionResult Index()
         {
+            // Example query
+            _dbConnection.Open();
+            var command = _dbConnection.CreateCommand();
+            command.CommandText = "SELECT * FROM Users";
+            var reader = command.ExecuteReader();
+
+            // Process the data...
+            while (reader.Read())
+            {
+                var userId = reader["User_ID"].ToString();
+                // Do something with the data...
+            }
+
+            _dbConnection.Close();
+
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
