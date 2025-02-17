bf1585b438bd023f61dd9795969719fe2a6f999d
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 16:39:34 +0000

    Create user.cs

---
+++ Models/user.cs
@@ -0,0 +1,13 @@
+namespace CampusJobsProject___Group_34.Models
+{
+    public class User
+    {
+        public int User_ID { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string Address { get; set; }
+    }
+}
