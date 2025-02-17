efb93b2111ad7f7344145dba454105aed5fcc030
MorganSHUni <c1026375@hallam.shu.ac.uk>
2025-02-17 06:06:40 +0000

    Added the files for the asp.net project
    This is a base for the project.
    Currently has example files included, that should be overwritten or replaced.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CampusJobsProject___Group_34.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
