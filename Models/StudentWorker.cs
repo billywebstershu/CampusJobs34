d0edc4692ec8750b22ccb0b8709e2d78b404bf31
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 16:42:03 +0000

    Create StudentWorker.cs

---
+++ Models/StudentWorker.cs
@@ -0,0 +1,9 @@
+namespace CampusJobsProject___Group_34.Models
+{
+    public class StudentWorker
+    {
+        public int VisaStatus_ID { get; set; }
+        public int Recruitment_ID { get; set; }
+        public Recruiter Recruiter { get; set; }
+    }
+}
