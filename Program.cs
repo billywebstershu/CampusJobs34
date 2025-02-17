15b9664cac3a9d6922b47d49326d8b64cb90ba5b
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-17 16:36:44 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+// Add DbContext for MySQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
