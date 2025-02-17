8eaceeeeb178b3b569003f591e02b9bdfdfcfb89
MorganSHUni <c1026375@hallam.shu.ac.uk>
2025-02-17 11:57:06 +0000

    Added JobModel.cs

---
+++ Models/JobModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CampusJobsProject___Group_34.Models
+{
+    public class JobModel
+    {
+        public int JobId { get; set; }
+        public int RecruiterId { get; set; }
+        public string Title { get; set; }
+        public string PostNumber { get; set; }
+        public decimal PayRate { get; set; }
+        public string CostCode { get; set; }
+        public int MaxAvailableHours { get; set; }
+    }
+}
