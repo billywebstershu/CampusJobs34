481c39692f2b3ac2a8976b7c63cdebfdf55ff611
Ebrahim0904 <Ebrahim.B.SaiyedMohammadAli@student.shu.ac.uk>
2025-02-20 11:53:59 +0000

    Add files via upload
    Okay nvm I got the other pages working too. They're still empty-ish, but I'll add stuff to them eventually.

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Dashboard.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Notifications";
+            return View();
+        }
+    }
+}
