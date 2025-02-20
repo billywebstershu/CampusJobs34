15d46e515872ed76fc192fcd1afcdb5df24e6000
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-20 13:37:55 +0000

    Update UserModel.cs

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,34 +1,13 @@
-﻿namespace CampusJobsProject___Group_34.Models
+namespace CampusJobsProject___Group_34.Models
 {
     public class UserModel
     {
         public int User_ID { get; set; }  
+        public string First_Name { get; set; }  
+        public string Last_Name { get; set; }  
         public string Email { get; set; }
         public string Password { get; set; }
         public int Role { get; set; } 
-        public string First_Name { get; set; }  
-        public string Last_Name { get; set; } 
         public string Address { get; set; } = string.Empty;
-    }
-
-    public class AdminModel : UserModel
-    {
-        public int Admin_ID { get; set; } 
-    }
-
-    public class StudentModel : UserModel
-    {
-        public int Student_ID { get; set; } 
-        public int VisaStatus_ID { get; set; }  
-    }
-
-    public class RecruiterModel : UserModel
-    {
-        public int Recruitment_ID { get; set; }  
-    }
-
-    public class EmployeeModel : StudentModel
-    {
-        public int JobId { get; set; }
     }
 }
