f83c48bbee030e9610e033b5c2f52cdd2eb5487c
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-20 13:55:56 +0000

    Update EmployeeModel.cs

--- Models/EmployeeModel.cs
+++ Models/EmployeeModel.cs
@@ -1,18 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace CampusJobsProject___Group_34.Models
 {
-    [Table("Employee")]
     public class EmployeeModel
     {
-        [Key]
-        [Column("Student_ID")]
         public int Student_ID { get; set; }
-
-        [ForeignKey("Recruiter")]
-        [Column("Recruitment_ID")]
         public int Recruitment_ID { get; set; }
     }
 }
-
