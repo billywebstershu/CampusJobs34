70a85d1e6c61769a119e0f126beb881f709ff6ae
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-20 10:05:32 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,11 +14,10 @@ builder.Services.AddTransient<IDbConnection>(_ => new MySqlConnection(connection
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error"); 
     app.UseHsts();
 }
 
