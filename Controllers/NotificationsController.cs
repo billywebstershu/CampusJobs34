47d74aee73384a78c040436b15ca937b009f6011
Ebrahim0904 <Ebrahim.B.SaiyedMohammadAli@student.shu.ac.uk>
2025-02-20 21:39:18 +0000

    Add files via upload
    Epic filter system and the accept and decline buttons do colours now. Notifications and Profile pages have something in them now. I'll leave the SQL thingie to Abdi. Godspeed.

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Student_Dashboard.Models;
+using System.Collections.Generic;
 
 namespace Student_Dashboard.Controllers
 {
@@ -6,8 +8,22 @@ namespace Student_Dashboard.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Title = "Notifications";
-            return View();
+            var notifications = new List<Notification>
+                {
+                    new Notification { Id = 1, Message = "Your shift for 2025-02-20 has been approved.", Time = "2025-02-19 10:00", Read = false },
+                    new Notification { Id = 2, Message = "Reminder: Shift ID S001 starts tomorrow.", Time = "2025-02-19 08:00", Read = false },
+                    new Notification { Id = 3, Message = "Your leave request has been denied.", Time = "2025-02-18 14:30", Read = true },
+                    new Notification { Id = 4, Message = "Your shift has been updated.", Time = "2025-02-18 09:00", Read = false },
+                    new Notification { Id = 5, Message = "A new recruiter has been assigned to your shift.", Time = "2025-02-17 16:00", Read = false },
+                    new Notification { Id = 6, Message = "Your shift start time has been adjusted.", Time = "2025-02-16 13:00", Read = false },
+                    new Notification { Id = 7, Message = "You have a new message from HR.", Time = "2025-02-15 11:00", Read = true },
+                    new Notification { Id = 8, Message = "Your request for shift change has been approved.", Time = "2025-02-14 10:30", Read = false },
+                    new Notification { Id = 9, Message = "Reminder: Your shift starts in 1 hour.", Time = "2025-02-20 08:30", Read = false },
+                    new Notification { Id = 10, Message = "You’ve been added to a new project team.", Time = "2025-02-13 17:15", Read = true }
+                };
+
+            return View(notifications);
         }
+
     }
 }
---
+++ Models/Notifications.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Dashboard.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Time { get; set; }
+        public bool Read { get; set; }
+    }
+}
