62ae33fff7b7f19431c38c8d5b4dd1080b067d4a
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-20 11:46:52 +0000

    Update UserModel.cs

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,7 +2,7 @@
 {
     public class UserModel
     {
-        public int UserId { get; set; }
+        public int User_ID { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
