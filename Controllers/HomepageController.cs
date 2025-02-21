065c13477d8fee69ca16f7b7850ce44ccbfdc373
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-02-21 09:28:54 +0000

    Add files via upload

---
+++ Controllers/HomepageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Dashboard.Controllers
+{
+    public class HomepageController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Homepage";
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Dashboard.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Profile";
+            return View();
+        }
+    }
+}
